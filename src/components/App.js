6cd88de61267ec61a3a8327f75ad723d83fb01c6
saivivek122 <119473212+saivivek122@users.noreply.github.com>
2025-10-03 20:58:22 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 
 import React from "react";
 import './../styles/App.css'; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Navigation from "./Navigation";
 
 const App = () => {
   return (
